62dc1c33e7209d96c2a58656701ffce4dc7e1f36
Samyak Maharjan <samyakmaharjan3@gmail.com>
2023-02-19 15:58:22 +0530

    Sum of even fibo

---
+++ evenfibonacchi.java
@@ -0,0 +1,21 @@
+public class evenfibonacchi{
+    static int fibo(int n){
+        if (n<=1) return n;
+        else return(fibo(n-1)+fibo(n-2));
+    }
+    public static void main(String[] args) {
+        int sum=0,fib,i=0;
+        boolean flag=true;
+        while(flag){
+            fib=fibo(i);
+            System.out.println(fib);
+            if((fib%2)==0){
+                sum+=fib;
+        }
+        if(fib>4000000) flag=false;
+        i++;
+        }
+        System.out.println("____________________________");
+        System.out.println(sum);
+    }
+}
